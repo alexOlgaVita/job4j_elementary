6514a8c6a467ffeb5da4dcd333c2a4d24a6c283d
olga.ilyina <olga.ilyina@axenix.pro>
2024-10-01 08:16:23 +0300

    Разбор первой программы по "взрослому" [#505151]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Olga Ilyina");
+        System.out.println("03.12.1977");
+    }
+}
