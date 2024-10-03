43302ab08bbcd1f69d5a9546e2da9efb7f556d90
olga.ilyina <olga.ilyina@axenix.pro>
2024-10-03 08:26:48 +0300

    Классический поиск перебором [#505227]

---
+++ src/test/java/ru/job4j/array/FindLoopTest.java
@@ -0,0 +1,43 @@
+package ru.job4j.array;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class FindLoopTest {
+    @Test
+    void whenArrayHas5Then0() {
+        int[] data = new int[] {5, 10, 3};
+        int element = 5;
+        int result = FindLoop.indexOf(data, element);
+        int expected = 0;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void whenArrayHas7Then2() {
+        int[] data = new int[] {1, 5, 7};
+        int element = 7;
+        int result = FindLoop.indexOf(data, element);
+        int expected = 2;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void whenArrayHas10Times2Then0() {
+        int[] data = new int[] {10, 13, 10};
+        int element = 10;
+        int result = FindLoop.indexOf(data, element);
+        int expected = 0;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void whenArrayHasNot10ThenMinus1() {
+        int[] data = new int[] {1, 11, 4};
+        int element = 10;
+        int result = FindLoop.indexOf(data, element);
+        int expected = -1;
+        assertThat(result).isEqualTo(expected);
+    }
+}
