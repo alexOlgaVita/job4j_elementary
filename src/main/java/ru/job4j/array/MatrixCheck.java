51079a10d08fd39224962e39170e9357d64f9cb5
olga.ilyina <olga.ilyina@axenix.pro>
2024-10-05 08:08:18 +0300

    6.7.4. Выигрышные комбинации в сокобан [#505248 #515791]

--- src/main/java/ru/job4j/array/MatrixCheck.java
+++ src/main/java/ru/job4j/array/MatrixCheck.java
@@ -4,10 +4,10 @@ public class MatrixCheck {
     public static boolean monoHorizontal(char[][] board, int row) {
         boolean result = true;
         for (int i = 0; i < board[row].length; i++) {
-                if ((board[row][i] != 'X')) {
-                    result = false;
-                    break;
-                }
+            if ((board[row][i] != 'X')) {
+                result = false;
+                break;
+            }
         }
         return result;
     }
@@ -33,12 +33,10 @@ public class MatrixCheck {
 
     public static boolean isWin(char[][] board) {
         boolean result = false;
-        for (int i = 0; i < 5; i++) {
-            if (board[i][i] == 'X') {
-                if ((monoHorizontal(board, i) || monoVertical(board, i))) {
-                    result = true;
-                    break;
-                }
+        for (int i = 0; i < board.length; i++) {
+            if ((board[i][i] == 'X') && ((monoHorizontal(board, i) || monoVertical(board, i)))) {
+                result = true;
+                break;
             }
         }
         return result;
