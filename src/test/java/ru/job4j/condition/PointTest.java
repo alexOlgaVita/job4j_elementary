17dcc962a98898accd97a08acb3ac0cde6d190e7
olga.ilyina <olga.ilyina@axenix.pro>
2024-10-05 15:22:24 +0300

    2.0. Рефакторинг - Расстояние между точками. [#122587]

--- src/test/java/ru/job4j/condition/PointTest.java
+++ src/test/java/ru/job4j/condition/PointTest.java
@@ -10,49 +10,55 @@ class PointTest {
 
     @Test
     void whenPoints00And20Then2() {
-        int x1 = 0, y1 = 0, x2 = 2, y2 = 0;
+        Point a = new Point(0, 0);
+        Point b = new Point(2, 0);
         double expected = 2.0;
-        double output = Point.distance(x1, y1, x2, y2);
+        double output = a.distance(b);
         assertThat(output).isEqualTo(expected, withPrecision(0.01));
     }
 
     @Test
     void whenPointsMinus1Minus1And1And1Then2Dot83() {
-        int x1 = -1, y1 = -1, x2 = 1, y2 = 1;
+        Point a = new Point(-1, -1);
+        Point b = new Point(1, 1);
         double expected = 2.83;
-        double output = Point.distance(x1, y1, x2, y2);
+        double output = a.distance(b);
         assertThat(output).isEqualTo(expected, withPrecision(0.01));
     }
 
     @Test
     void whenPointsMinus2Minus2And2And2Then5Dot66() {
-        int x1 = -2, y1 = -2, x2 = 2, y2 = 2;
+        Point a = new Point(-2, -2);
+        Point b = new Point(2, 2);
         double expected = 5.66;
-        double output = Point.distance(x1, y1, x2, y2);
+        double output = a.distance(b);
         assertThat(output).isEqualTo(expected, withPrecision(0.01));
     }
 
     @Test
     void whenPointsMinus77Minus2AndMinus23And0Then54Dot04() {
-        int x1 = -77, y1 = -2, x2 = -23, y2 = 0;
+        Point a = new Point(-77, -2);
+        Point b = new Point(-23, 0);
         double expected = 54.04;
-        double output = Point.distance(x1, y1, x2, y2);
+        double output = a.distance(b);
         assertThat(output).isEqualTo(expected, withPrecision(0.01));
     }
 
     @Test
     void whenPoints99AndMinus9AndMinus9Then25Dot46() {
-        int x1 = 9, y1 = 9, x2 = -9, y2 = -9;
+        Point a = new Point(9, 9);
+        Point b = new Point(-9, -9);
         double expected = 25.46;
-        double output = Point.distance(x1, y1, x2, y2);
+        double output = a.distance(b);
         assertThat(output).isEqualTo(expected, withPrecision(0.01));
     }
 
     @Test
     void whenPointsMinus7And6And9AndMinus7Then20Dot62() {
-        int x1 = -7, y1 = 6, x2 = 9, y2 = -7;
+        Point a = new Point(-7, 6);
+        Point b = new Point(9, -7);
         double expected = 20.62;
-        double output = Point.distance(x1, y1, x2, y2);
+        double output = a.distance(b);
         assertThat(output).isEqualTo(expected, withPrecision(0.01));
     }
 }
